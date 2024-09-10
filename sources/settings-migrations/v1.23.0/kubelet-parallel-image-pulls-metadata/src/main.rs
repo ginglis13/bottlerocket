04bd4f223bd19f138e1104e8ab7622c03ec68a6f
Gavin Inglis <giinglis@amazon.com>
2024-09-10 00:06:19 +0000

    migrations: add migrations for kubelet paralell image pulls
    see https://github.com/bottlerocket-os/bottlerocket/pull/2986/files#diff-68d67ac82475a00594b8bfc3c99e5fef80fba1474bc463ff7cacedb97e64958d
    
    Signed-off-by: Gavin Inglis <giinglis@amazon.com>

---
+++ sources/settings-migrations/v1.23.0/kubelet-parallel-image-pulls-metadata/src/main.rs
@@ -0,0 +1,22 @@
+{AddMetadataMigration, SettingMetadata};
+use migration_helpers::migrate;
+use migration_helpers::Result;
+use std::process;
+
+/// We added a new setting for configuring kubelet
+fn run() -> Result<()> {
+    migrate(AddMetadataMigration(&[SettingMetadata {
+        metadata: &["affected-services"],
+        setting: "settings.kubernetes",
+    }]))
+}
+
+// Returning a Result from main makes it print a Debug representation of the error, but with Snafu
+// we have nice Display representations of the error, so we wrap "main" (run) and print any error.
+// https://github.com/shepmaster/snafu/issues/110
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
